5a4ccc297f6f2ee5e4bd2fcdedb143e69d44bbde
Adryancosmo56@gmail.com <Vikin.021>
2022-08-10 21:43:37 -0300

    Conectando com o banco de dados

--- BackEnd_Dev/src/app.js
+++ BackEnd_Dev/src/app.js
@@ -6,13 +6,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json);
-//const router = express.Router(); 
+const router = express.Router();
 
 //rotas
 const index = require('./routes/index');
 const apiRouter = require('./routes/apiRoute');
-const produtosRoute = require('./routes/produtosRoute');
+//const produtosRoute = require('./routes/produtosRoute');
 app.use('/', index);
 app.use('/rotas', apiRouter);
-app.use('/produtos', produtosRoute);
+//app.use('/produtos', produtosRoute);
 module.exports = app;
--- BackEnd_Dev/src/routes/apiRoute.js
+++ BackEnd_Dev/src/routes/apiRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/controller');
 
-router.post('/', controller.post); dadasdas
-router.put('/:id/:nome', controller.put);
-router.delete('/:id/:nome', controller.delete);
+router.post('/', controller.post);
+router.put('/:id/', controller.put);
+router.delete('/:id/', controller.delete);
 module.exports = router;
